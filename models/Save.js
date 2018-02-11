0e5cdc8f0ae66b0e0553e50c8ad1134d4b90fac2
Shant Wanes <swanes@bjsrestaurants.com>
2018-02-11 10:03:17 -0800

    Models2

---
+++ models/Save.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Create article schema
+var SaveSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+  },
+   byline: {
+   type: String,
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Save model with the SaveSchema
+var Save = mongoose.model("Save", SaveSchema);
+
+// Export the model
+module.exports = Save;
